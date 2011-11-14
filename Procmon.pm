77aecdda666e3b9e56ea9da28d8d351bfd2baba5
Richard Harman <richard@richardharman.com>
2011-11-13 23:42:32 -0500

    Adding file command

--- Procmon.pm
+++ Procmon.pm
@@ -95,6 +95,7 @@ sub newpids {
       -input => pod_where({-inc => 1}, __PACKAGE__),
     );
   } # }}}
+
 format STDOUT_TOP =
  IDx   PID  PPID Command
 .
@@ -202,6 +203,59 @@ sub skeleton {
   my $xml = $self->doc;
   my $nodelist = $xml->findnodes($opts->{expression});
   print map { $_->toString(2),"\n" } $nodelist->get_nodelist;
+} # }}}
+
+# COMMAND: files {{{
+
+=head2 files
+
+Show file activity from a process
+
+  files --pid 404
+
+=cut
+
+sub files {
+  my ($self,@args) = @_;
+  my $opts = {} ;
+  my $ret = GetOptions($opts,"help|?",
+    "pid=i",
+  );
+  if ($opts->{help}) { # {{{
+    pod2usage(
+      -msg=> "skeleton Help ",
+      -verbose => 99,
+      -sections => [ qw(COMMANDS/skeleton) ],
+      -exitval=>0,
+      -input => pod_where({-inc => 1}, __PACKAGE__),
+    );
+  } # }}}
+
+  my $xml = $self->doc;
+  my $xpath = sprintf('/procmon/eventlist/event[PID=%d and contains(./Operation,"File")]',$opts->{pid});
+  my $nodelist = $xml->findnodes($xpath);
+  my $hash = {};
+
+format STDOUT_TOP =
+ IDx  PID Process          Operation               Result
+.
+
+format STDOUT =
+@>>> @>>> @<<<<<<<<<<<<<<< @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<... @<<<<<<<<<<<<<<<
+@$hash{qw(IDx PID Process_Name Operation Result)}
+Path: @*
+$hash->{Path}
+Detail: ^*~~
+@$hash{Detail}
+---------------------------------------------------------------------------
+
+.
+
+  foreach my $node ($nodelist->get_nodelist) {
+    @$hash{qw(IDx PID Process_Name Operation Result Path Detail)} = map { $node->findvalue($_) } qw(./ProcessIndex ./PID ./Process_Name ./Operation ./Result ./Path ./Detail);
+    write;
+  }
+  #print map { $_->toString(2),"\n" } $nodelist->get_nodelist;
 } # }}}
 
 1;
